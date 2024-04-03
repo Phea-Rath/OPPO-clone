481eac452222c3047da11157396966b98441099f
Cooper Premium <160566976+Phea-Rath@users.noreply.github.com>
2024-04-03 12:02:46 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,554 @@
+function Click() {
+  let Chat = document.getElementById("chat");
+  Chat.classList.toggle("contect");
+}
+function Go() {
+  let Angle = document.getElementById("step");
+  Angle.classList.toggle("to-right");
+}
+
+function Goto() {
+  let Angle = document.getElementById("step");
+  Angle.classList.toggle("to-left");
+}
+
+function Go1() {
+  let Angle = document.getElementById("step1");
+  Angle.classList.toggle("to-right");
+}
+
+function Goto1() {
+  let Angle = document.getElementById("step1");
+  Angle.classList.toggle("to-left");
+}
+
+let Soum = document.getElementById("soum");
+let Soum1 = document.getElementById("soum1");
+let Soum2 = document.getElementById("soum2");
+let Soum3 = document.getElementById("soum3");
+let Soum4 = document.getElementById("soum4");
+let Soum5 = document.getElementById("soum5");
+let Soum6 = document.getElementById("soum6");
+let Soum7 = document.getElementById("soum7");
+let Soum8 = document.getElementById("soum8");
+let Soum9 = document.getElementById("soum9");
+let Soum10 = document.getElementById("soum10");
+let Soum11 = document.getElementById("soum11");
+let Soum12 = document.getElementById("soum12");
+let Soum13 = document.getElementById("soum13");
+let Soum14 = document.getElementById("soum14");
+let Soum15 = document.getElementById("soum15");
+let Soum16 = document.getElementById("soum16");
+let Soum17 = document.getElementById("soum17");
+let Soum18 = document.getElementById("soum18");
+let Soum19 = document.getElementById("soum19");
+let Soum20 = document.getElementById("soum20");
+let Soum21 = document.getElementById("soum21");
+let Soum22 = document.getElementById("soum22");
+let Soum23 = document.getElementById("soum23");
+let Soum24 = document.getElementById("soum24");
+let Soum25 = document.getElementById("soum25");
+let Soum26 = document.getElementById("soum26");
+let Soum27 = document.getElementById("soum27");
+let Soum28 = document.getElementById("soum28");
+let Soum29 = document.getElementById("soum29");
+let Soum30 = document.getElementById("soum30");
+let Soum31 = document.getElementById("soum31");
+let Soum32 = document.getElementById("soum32");
+let Soum33 = document.getElementById("soum33");
+let Soum34 = document.getElementById("soum34");
+let Soum35 = document.getElementById("soum35");
+let Soum36 = document.getElementById("soum36");
+let Soum37 = document.getElementById("soum37");
+let Soum38 = document.getElementById("soum38");
+let Soum39 = document.getElementById("soum39");
+let Soum40 = document.getElementById("soum40");
+let Soum42 = document.getElementById("soum41");
+let Soum41 = document.getElementById("soum42");
+let Soum43 = document.getElementById("soum43");
+let Phone = document.getElementById("phone1");
+let Phone1 = document.getElementById("phone2");
+let Phone2 = document.getElementById("phone3");
+let Phone3 = document.getElementById("phone4");
+let Phone4 = document.getElementById("phone5");
+let Phone5 = document.getElementById("phone6");
+let Phone6 = document.getElementById("phone7");
+let Phone7 = document.getElementById("phone8");
+let Phone8 = document.getElementById("phone9");
+let Phone9 = document.getElementById("phone10");
+let Phone10 = document.getElementById("phone11");
+let Phone11 = document.getElementById("phone12");
+let Phone12 = document.getElementById("phone13");
+let Phone13 = document.getElementById("phone14");
+let Phone14 = document.getElementById("phone15");
+let Phone15 = document.getElementById("phone16");
+let Phone16 = document.getElementById("phone17");
+let Phone17 = document.getElementById("phone18");
+let Phone18 = document.getElementById("phone19");
+let Phone19 = document.getElementById("phone20");
+let Phone20 = document.getElementById("phone21");
+let Phone21 = document.getElementById("phone22");
+let Phone22 = document.getElementById("phone23");
+let Phone23 = document.getElementById("phone24");
+let Phone24 = document.getElementById("phone25");
+let Phone25 = document.getElementById("phone26");
+let Phone26 = document.getElementById("phone27");
+let Phone27 = document.getElementById("phone28");
+let Phone28 = document.getElementById("phone29");
+let Phone29 = document.getElementById("phone30");
+let Phone30 = document.getElementById("phone31");
+let Phone31 = document.getElementById("phone32");
+let Phone32 = document.getElementById("phone33");
+let Phone33 = document.getElementById("phone34");
+let Phone34 = document.getElementById("phone35");
+let Phone35 = document.getElementById("phone36");
+let Phone36 = document.getElementById("phone37");
+let Phone37 = document.getElementById("phone38");
+let Phone38 = document.getElementById("phone39");
+let Phone39 = document.getElementById("phone40");
+let Phone40 = document.getElementById("phone41");
+let Phone41 = document.getElementById("phone42");
+let Phone42 = document.getElementById("phone43");
+let Phone43 = document.getElementById("phone44");
+
+document.getElementById("dot").onclick = function () {
+  Soum.style.width = "25px";
+  Soum.style.height = "25px";
+  Soum.style.border = "2px solid #afadad";
+  Phone.style.zIndex = "2";
+  Soum1.style.width = "20px";
+  Soum1.style.border = "none";
+  Soum1.style.height = "20px";
+  Phone1.style.zIndex = "1";
+};
+document.getElementById("dot2").onclick = function () {
+  Soum2.style.width = "25px";
+  Soum2.style.height = "25px";
+  Soum2.style.border = "2px solid #afadad";
+  Phone2.style.zIndex = "2";
+  Soum3.style.width = "20px";
+  Soum3.style.border = "none";
+  Soum3.style.height = "20px";
+  Phone3.style.zIndex = "1";
+};
+document.getElementById("dot4").onclick = function () {
+  Soum4.style.width = "25px";
+  Soum4.style.height = "25px";
+  Soum4.style.border = "2px solid #afadad";
+  Phone4.style.zIndex = "2";
+  Soum5.style.width = "20px";
+  Soum5.style.border = "none";
+  Soum5.style.height = "20px";
+  Phone5.style.zIndex = "1";
+};
+document.getElementById("dot6").onclick = function () {
+  Soum6.style.width = "25px";
+  Soum6.style.height = "25px";
+  Soum6.style.border = "2px solid #afadad";
+  Phone6.style.zIndex = "2";
+  Soum7.style.width = "20px";
+  Soum7.style.border = "none";
+  Soum7.style.height = "20px";
+  Phone7.style.zIndex = "1";
+};
+document.getElementById("dot8").onclick = function () {
+  Soum8.style.width = "25px";
+  Soum8.style.height = "25px";
+  Soum8.style.border = "2px solid #afadad";
+  Phone8.style.zIndex = "2";
+  Soum9.style.width = "20px";
+  Soum9.style.border = "none";
+  Soum9.style.height = "20px";
+  Phone9.style.zIndex = "1";
+};
+document.getElementById("dot10").onclick = function () {
+  Soum10.style.width = "25px";
+  Soum10.style.height = "25px";
+  Soum10.style.border = "2px solid #afadad";
+  Phone10.style.zIndex = "2";
+  Soum11.style.width = "20px";
+  Soum11.style.border = "none";
+  Soum11.style.height = "20px";
+  Phone11.style.zIndex = "1";
+};
+document.getElementById("dot12").onclick = function () {
+  Soum12.style.width = "25px";
+  Soum12.style.height = "25px";
+  Soum12.style.border = "2px solid #afadad";
+  Phone12.style.zIndex = "2";
+  Soum13.style.width = "20px";
+  Soum13.style.border = "none";
+  Soum13.style.height = "20px";
+  Phone13.style.zIndex = "1";
+};
+document.getElementById("dot14").onclick = function () {
+  Soum14.style.width = "25px";
+  Soum14.style.height = "25px";
+  Soum14.style.border = "2px solid #afadad";
+  Phone14.style.zIndex = "2";
+  Soum15.style.width = "20px";
+  Soum15.style.border = "none";
+  Soum15.style.height = "20px";
+  Phone15.style.zIndex = "1";
+};
+document.getElementById("dot16").onclick = function () {
+  Soum16.style.width = "25px";
+  Soum16.style.height = "25px";
+  Soum16.style.border = "2px solid #afadad";
+  Phone16.style.zIndex = "2";
+  Soum17.style.width = "20px";
+  Soum17.style.border = "none";
+  Soum17.style.height = "20px";
+  Phone17.style.zIndex = "1";
+};
+document.getElementById("dot18").onclick = function () {
+  Soum18.style.width = "25px";
+  Soum18.style.height = "25px";
+  Soum18.style.border = "2px solid #afadad";
+  Phone18.style.zIndex = "2";
+  Soum19.style.width = "20px";
+  Soum19.style.border = "none";
+  Soum19.style.height = "20px";
+  Phone19.style.zIndex = "1";
+};
+document.getElementById("dot20").onclick = function () {
+  Soum20.style.width = "25px";
+  Soum20.style.height = "25px";
+  Soum20.style.border = "2px solid #afadad";
+  Phone20.style.zIndex = "2";
+  Soum21.style.width = "20px";
+  Soum21.style.border = "none";
+  Soum21.style.height = "20px";
+  Phone21.style.zIndex = "1";
+};
+document.getElementById("dot22").onclick = function () {
+  Soum22.style.width = "25px";
+  Soum22.style.height = "25px";
+  Soum22.style.border = "2px solid #afadad";
+  Phone22.style.zIndex = "2";
+  Soum23.style.width = "20px";
+  Soum23.style.border = "none";
+  Soum23.style.height = "20px";
+  Phone23.style.zIndex = "1";
+};
+document.getElementById("dot24").onclick = function () {
+  Soum24.style.width = "25px";
+  Soum24.style.height = "25px";
+  Soum24.style.border = "2px solid #afadad";
+  Phone24.style.zIndex = "2";
+  Soum25.style.width = "20px";
+  Soum25.style.border = "none";
+  Soum25.style.height = "20px";
+  Phone25.style.zIndex = "1";
+};
+document.getElementById("dot26").onclick = function () {
+  Soum26.style.width = "25px";
+  Soum26.style.height = "25px";
+  Soum26.style.border = "2px solid #afadad";
+  Phone26.style.zIndex = "2";
+  Soum27.style.width = "20px";
+  Soum27.style.border = "none";
+  Soum27.style.height = "20px";
+  Phone27.style.zIndex = "1";
+};
+document.getElementById("dot28").onclick = function () {
+  Soum28.style.width = "25px";
+  Soum28.style.height = "25px";
+  Soum28.style.border = "2px solid #afadad";
+  Phone28.style.zIndex = "2";
+  Soum29.style.width = "20px";
+  Soum29.style.border = "none";
+  Soum29.style.height = "20px";
+  Phone29.style.zIndex = "1";
+};
+document.getElementById("dot30").onclick = function () {
+  Soum30.style.width = "25px";
+  Soum30.style.height = "25px";
+  Soum30.style.border = "2px solid #afadad";
+  Phone30.style.zIndex = "2";
+  Soum31.style.width = "20px";
+  Soum31.style.border = "none";
+  Soum31.style.height = "20px";
+  Phone31.style.zIndex = "1";
+};
+document.getElementById("dot32").onclick = function () {
+  Soum32.style.width = "25px";
+  Soum32.style.height = "25px";
+  Soum32.style.border = "2px solid #afadad";
+  Phone32.style.zIndex = "2";
+  Soum33.style.width = "20px";
+  Soum33.style.border = "none";
+  Soum33.style.height = "20px";
+  Phone33.style.zIndex = "1";
+};
+document.getElementById("dot34").onclick = function () {
+  Soum34.style.width = "25px";
+  Soum34.style.height = "25px";
+  Soum34.style.border = "2px solid #afadad";
+  Phone34.style.zIndex = "2";
+  Soum35.style.width = "20px";
+  Soum35.style.border = "none";
+  Soum35.style.height = "20px";
+  Phone35.style.zIndex = "1";
+};
+document.getElementById("dot36").onclick = function () {
+  Soum36.style.width = "25px";
+  Soum36.style.height = "25px";
+  Soum36.style.border = "2px solid #afadad";
+  Phone36.style.zIndex = "2";
+  Soum37.style.width = "20px";
+  Soum37.style.border = "none";
+  Soum37.style.height = "20px";
+  Phone37.style.zIndex = "1";
+};
+document.getElementById("dot38").onclick = function () {
+  Soum38.style.width = "25px";
+  Soum38.style.height = "25px";
+  Soum38.style.border = "2px solid #afadad";
+  Phone38.style.zIndex = "2";
+  Soum39.style.width = "20px";
+  Soum39.style.border = "none";
+  Soum39.style.height = "20px";
+  Phone39.style.zIndex = "1";
+};
+document.getElementById("dot40").onclick = function () {
+  Soum40.style.width = "25px";
+  Soum40.style.height = "25px";
+  Soum40.style.border = "2px solid #afadad";
+  Phone40.style.zIndex = "2";
+  Soum41.style.width = "20px";
+  Soum41.style.border = "none";
+  Soum41.style.height = "20px";
+  Phone41.style.zIndex = "1";
+};
+document.getElementById("dot42").onclick = function () {
+  Soum42.style.width = "25px";
+  Soum42.style.height = "25px";
+  Soum42.style.border = "2px solid #afadad";
+  Phone42.style.zIndex = "2";
+  Soum43.style.width = "20px";
+  Soum43.style.border = "none";
+  Soum43.style.height = "20px";
+  Phone43.style.zIndex = "1";
+};
+//---------------------------------------------------------------------->
+document.getElementById("dot1").onclick = function () {
+  Soum1.style.width = "25px";
+  Soum1.style.height = "25px";
+  Soum1.style.border = "2px solid #afadad";
+  Phone1.style.zIndex = "2";
+  Soum.style.width = "25px";
+  Soum.style.border = "none";
+  Soum.style.height = "25px";
+  Phone.style.zIndex = "1";
+};
+document.getElementById("dot3").onclick = function () {
+  Soum3.style.width = "25px";
+  Soum3.style.height = "25px";
+  Soum3.style.border = "2px solid #afadad";
+  Phone3.style.zIndex = "2";
+  Soum2.style.width = "20px";
+  Soum2.style.border = "none";
+  Soum2.style.height = "20px";
+  Phone2.style.zIndex = "1";
+};
+document.getElementById("dot5").onclick = function () {
+  Soum5.style.width = "25px";
+  Soum5.style.height = "25px";
+  Soum5.style.border = "2px solid #afadad";
+  Phone5.style.zIndex = "2";
+  Soum4.style.width = "20px";
+  Soum4.style.border = "none";
+  Soum4.style.height = "20px";
+  Phone4.style.zIndex = "1";
+};
+document.getElementById("dot7").onclick = function () {
+  Soum7.style.width = "25px";
+  Soum7.style.height = "25px";
+  Soum7.style.border = "2px solid #afadad";
+  Phone7.style.zIndex = "2";
+  Soum6.style.width = "20px";
+  Soum6.style.border = "none";
+  Soum6.style.height = "20px";
+  Phone6.style.zIndex = "1";
+};
+document.getElementById("dot9").onclick = function () {
+  Soum9.style.width = "25px";
+  Soum9.style.height = "25px";
+  Soum9.style.border = "2px solid #afadad";
+  Phone9.style.zIndex = "2";
+  Soum8.style.width = "20px";
+  Soum8.style.border = "none";
+  Soum8.style.height = "20px";
+  Phone8.style.zIndex = "1";
+};
+document.getElementById("dot11").onclick = function () {
+  Soum11.style.width = "25px";
+  Soum11.style.height = "25px";
+  Soum11.style.border = "2px solid #afadad";
+  Phone11.style.zIndex = "2";
+  Soum10.style.width = "20px";
+  Soum10.style.border = "none";
+  Soum10.style.height = "20px";
+  Phone10.style.zIndex = "1";
+};
+document.getElementById("dot13").onclick = function () {
+  Soum13.style.width = "25px";
+  Soum13.style.height = "25px";
+  Soum13.style.border = "2px solid #afadad";
+  Phone13.style.zIndex = "2";
+  Soum12.style.width = "20px";
+  Soum12.style.border = "none";
+  Soum12.style.height = "20px";
+  Phone12.style.zIndex = "1";
+};
+document.getElementById("dot15").onclick = function () {
+  Soum15.style.width = "25px";
+  Soum15.style.height = "25px";
+  Soum15.style.border = "2px solid #afadad";
+  Phone15.style.zIndex = "2";
+  Soum14.style.width = "20px";
+  Soum14.style.border = "none";
+  Soum14.style.height = "20px";
+  Phone14.style.zIndex = "1";
+};
+document.getElementById("dot17").onclick = function () {
+  Soum17.style.width = "25px";
+  Soum17.style.height = "25px";
+  Soum17.style.border = "2px solid #afadad";
+  Phone17.style.zIndex = "2";
+  Soum16.style.width = "20px";
+  Soum16.style.border = "none";
+  Soum16.style.height = "20px";
+  Phone16.style.zIndex = "1";
+};
+document.getElementById("dot19").onclick = function () {
+  Soum19.style.width = "25px";
+  Soum19.style.height = "25px";
+  Soum19.style.border = "2px solid #afadad";
+  Phone19.style.zIndex = "2";
+  Soum18.style.width = "20px";
+  Soum18.style.border = "none";
+  Soum18.style.height = "20px";
+  Phone18.style.zIndex = "1";
+};
+document.getElementById("dot21").onclick = function () {
+  Soum21.style.width = "25px";
+  Soum21.style.height = "25px";
+  Soum21.style.border = "2px solid #afadad";
+  Phone21.style.zIndex = "2";
+  Soum20.style.width = "20px";
+  Soum20.style.border = "none";
+  Soum20.style.height = "20px";
+  Phone20.style.zIndex = "1";
+};
+document.getElementById("dot23").onclick = function () {
+  Soum23.style.width = "25px";
+  Soum23.style.height = "25px";
+  Soum23.style.border = "2px solid #afadad";
+  Phone23.style.zIndex = "2";
+  Soum22.style.width = "20px";
+  Soum22.style.border = "none";
+  Soum22.style.height = "20px";
+  Phone22.style.zIndex = "1";
+};
+document.getElementById("dot25").onclick = function () {
+  Soum25.style.width = "25px";
+  Soum25.style.height = "25px";
+  Soum25.style.border = "2px solid #afadad";
+  Phone25.style.zIndex = "2";
+  Soum24.style.width = "20px";
+  Soum24.style.border = "none";
+  Soum24.style.height = "20px";
+  Phone24.style.zIndex = "1";
+};
+document.getElementById("dot27").onclick = function () {
+  Soum27.style.width = "25px";
+  Soum27.style.height = "25px";
+  Soum27.style.border = "2px solid #afadad";
+  Phone27.style.zIndex = "2";
+  Soum26.style.width = "20px";
+  Soum26.style.border = "none";
+  Soum26.style.height = "20px";
+  Phone26.style.zIndex = "1";
+};
+document.getElementById("dot29").onclick = function () {
+  Soum29.style.width = "25px";
+  Soum29.style.height = "25px";
+  Soum29.style.border = "2px solid #afadad";
+  Phone29.style.zIndex = "2";
+  Soum28.style.width = "20px";
+  Soum28.style.border = "none";
+  Soum28.style.height = "20px";
+  Phone28.style.zIndex = "1";
+};
+document.getElementById("dot31").onclick = function () {
+  Soum31.style.width = "25px";
+  Soum31.style.height = "25px";
+  Soum31.style.border = "2px solid #afadad";
+  Phone31.style.zIndex = "2";
+  Soum30.style.width = "20px";
+  Soum30.style.border = "none";
+  Soum30.style.height = "20px";
+  Phone30.style.zIndex = "1";
+};
+document.getElementById("dot33").onclick = function () {
+  Soum33.style.width = "25px";
+  Soum33.style.height = "25px";
+  Soum33.style.border = "2px solid #afadad";
+  Phone33.style.zIndex = "2";
+  Soum32.style.width = "20px";
+  Soum32.style.border = "none";
+  Soum32.style.height = "20px";
+  Phone32.style.zIndex = "1";
+};
+document.getElementById("dot35").onclick = function () {
+  Soum35.style.width = "25px";
+  Soum35.style.height = "25px";
+  Soum35.style.border = "2px solid #afadad";
+  Phone35.style.zIndex = "2";
+  Soum34.style.width = "20px";
+  Soum34.style.border = "none";
+  Soum34.style.height = "20px";
+  Phone34.style.zIndex = "1";
+};
+document.getElementById("dot37").onclick = function () {
+  Soum37.style.width = "25px";
+  Soum37.style.height = "25px";
+  Soum37.style.border = "2px solid #afadad";
+  Phone37.style.zIndex = "2";
+  Soum36.style.width = "20px";
+  Soum36.style.border = "none";
+  Soum36.style.height = "20px";
+  Phone36.style.zIndex = "1";
+};
+document.getElementById("dot39").onclick = function () {
+  Soum39.style.width = "25px";
+  Soum39.style.height = "25px";
+  Soum39.style.border = "2px solid #afadad";
+  Phone39.style.zIndex = "2";
+  Soum38.style.width = "20px";
+  Soum38.style.border = "none";
+  Soum38.style.height = "20px";
+  Phone38.style.zIndex = "1";
+};
+document.getElementById("dot41").onclick = function () {
+  Soum41.style.width = "25px";
+  Soum41.style.height = "25px";
+  Soum41.style.border = "2px solid #afadad";
+  Phone41.style.zIndex = "2";
+  Soum40.style.width = "20px";
+  Soum40.style.border = "none";
+  Soum40.style.height = "20px";
+  Phone40.style.zIndex = "1";
+};
+document.getElementById("dot43").onclick = function () {
+  Soum43.style.width = "25px";
+  Soum43.style.height = "25px";
+  Soum43.style.border = "2px solid #afadad";
+  Phone43.style.zIndex = "2";
+  Soum42.style.width = "20px";
+  Soum42.style.border = "none";
+  Soum42.style.height = "20px";
+  Phone42.style.zIndex = "1";
+};
